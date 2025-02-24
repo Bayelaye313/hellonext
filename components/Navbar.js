24ee9cd0f269cb488c44cf4422aea3cc4e6bcdfa
bayelaye313 <bayelaye0510@gmail.com>
2025-02-24 17:33:53 +0000

    feat: navbar & links

---
+++ components/Navbar.js
@@ -0,0 +1,55 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { auth, signIn, signOut } from "../auth";
+
+const Navbar = async () => {
+  const session = await auth();
+
+  return (
+    <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
+      <nav className="flex justify-between items-center ">
+        <Link href="/">
+          <Image src="/logo2.avif" alt="logo" width={70} height={20} />
+        </Link>
+
+        <div className="flex items-center gap-5 text-black ">
+          {session && session?.user ? (
+            <>
+              <Link href="/startup/create">
+                {" "}
+                <span>Create</span>{" "}
+              </Link>
+              <form
+                action={async () => {
+                  "use server";
+                  await signOut({ redirectTo: "/" });
+                }}
+              >
+                <button type="submit">
+                  <span>Logout</span>
+                </button>
+              </form>
+              <Link href={`/user/${session?.id}`}>
+                {" "}
+                <span>{session?.user?.name}</span>{" "}
+              </Link>
+            </>
+          ) : (
+            <form
+              action={async () => {
+                "use server";
+
+                await signIn("github");
+              }}
+            >
+              <button type="submit">Login</button>
+            </form>
+          )}
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
