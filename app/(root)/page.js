7d1e58c6dce48cbe74eae987727b313630c253ea
bayelaye313 <bayelaye0510@gmail.com>
2025-02-25 11:49:25 +0000

    feat hero section

--- app/(root)/page.js
+++ app/(root)/page.js
@@ -1,7 +1,25 @@
-export default function Home() {
+import SearchForm from "../../components/SearchForm";
+
+export default async function Home({ searchParams }) {
+  const query = (await searchParams).query;
   return (
     <>
-      <h1 className="font-bold text-red-900">HOme</h1>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch Your Startup, <br />
+          Connect With Entrepreneurs
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
+          Competitions.
+        </p>
+        <SearchForm query={query} />
+      </section>
+      <section className="section_container">
+        <p className="text-30-emibold">
+          {query ? `Search result for ${query}` : "all startups"}
+        </p>
+      </section>
     </>
   );
 }
